45bb122b2a9400fde688db0c6f6f8d6b8adf92a9
Walter Castro <waltercstr28@gmail.com>
2023-04-13 13:04:58 -0700

    updated javascript, scss, some html

--- js/main.js
+++ js/main.js
@@ -1,16 +1,48 @@
-function showFortune() {
-    const fortunes = [
-      { fortune: "It is certain.", image:"images/magic8ball_1.png" },
-      { fortune: "It is decidedly so.", image: "images/magic8ball_2.png" },
-      { fortune: "Without a doubt.", image: "images/magic8ball_3.png" }
-    ];
+let fortunes = [
+  {
+    fortune: "It is certain.",
+    images: [
+      "images/magic8ball_1.png",
+      "images/magic8ball_4.png",
+      "images/magic8ball_5.png",
+    ],
+  },
+  {
+    fortune: "It is decidedly so.",
+    images: [
+      "images/magic8ball_2.png",
+      "images/magic8ball_6.png",
+      "images/magic8ball_7.png",
+    ],
+  },
+  {
+    fortune: "Without a doubt.",
+    images: [
+      "images/magic8ball_3.png",
+      "images/magic8ball_8.png",
+      "images/magic8ball_9.png",
+    ],
+  },
+  {
+    fortune: "Cannot predict now.",
+    images: [
+      "images/magic8ball_start.png",
+    ],
+  },
+];
+
+
+const showFortune = () => {
+  const randomIndex = Math.floor(Math.random() * fortunes.length);
+  const { fortune, images } = fortunes[randomIndex];
   
-    let randomIndex = Math.floor(Math.random() * fortunes.length);
-    let fortune = fortunes[randomIndex];
-  
-    let fortuneDiv = document.getElementById("fortune");
-    let imageElement = document.createElement("img");
-    imageElement.src = fortune.image;
-    fortuneDiv.innerHTML = "<h2>Your Fortune:</h2><p>" + fortune.fortune + "</p>";
-    fortuneDiv.appendChild(imageElement);
-  }
+  const randomImageIndex = Math.floor(Math.random() * images.length);
+  const imagePath = images[randomImageIndex];
+
+  const fortuneDiv = document.getElementById("fortune");
+  const imageElement = document.createElement("img");
+  imageElement.src = imagePath;
+  fortuneDiv.innerHTML = `<h2>Your Fortune:</h2><p>${fortune}</p>`;
+  fortuneDiv.appendChild(imageElement);
+};
+
